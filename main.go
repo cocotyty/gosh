7e11bcc9da23290745709504ad1708787de3febb
cocotyty <cocotyty@sina.com>
2017-09-28 19:25:06 +0800

    update

--- main.go
+++ main.go
@@ -4,17 +4,20 @@ import (
 	"fmt"
 	"github.com/elazarl/goproxy"
 	"github.com/shadowsocks/shadowsocks-go/shadowsocks"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
+	"strconv"
+	"time"
 )
 
 const GOSH_ENV = "SS_SERVER"
 
 func main() {
 	// start ss http proxy
-	startSSClient()
+	port := startSSClient()
 	// copy cmd to go
 	cmd := exec.Command("go", os.Args[1:]...)
 	cmd.Stdin = os.Stdin
@@ -22,14 +25,14 @@ func main() {
 	cmd.Stderr = os.Stderr
 	wd, _ := os.Getwd()
 	cmd.Dir = wd
-	cmd.Env = append(os.Environ(), "HTTP_PROXY=http://127.0.0.1:30000", "HTTPS_PROXY=http://127.0.0.1:30000")
+	cmd.Env = append(os.Environ(), "HTTP_PROXY=http://127.0.0.1:"+strconv.Itoa(port), "HTTPS_PROXY=http://127.0.0.1:"+strconv.Itoa(port))
 	err := cmd.Run()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
 	}
 }
-func startSSClient() {
+func startSSClient() (port int) {
 
 	ssServer := os.Getenv(GOSH_ENV)
 
@@ -57,12 +60,36 @@ func startSSClient() {
 
 	server.ConnectDial = dialer.Dial
 	server.Tr.Dial = dialer.Dial
-	go listenHTTPProxy(server)
+	return listenHTTPProxy(server)
 }
-func listenHTTPProxy(server http.Handler) {
-	err := http.ListenAndServe(":30000", server)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
+
+func listenHTTPProxy(server http.Handler) (port int) {
+	var ln net.Listener
+	var err error
+	for i := 0; i < 65535-3000; i++ {
+		port = 3000 + i
+		ln, err = net.Listen("tcp", ":"+strconv.Itoa(port))
+		if err == nil {
+			break
+		}
 	}
+	if err != nil {
+		panic("cannot found a port to listen")
+	}
+	go http.Serve(tcpKeepAliveListener{ln.(*net.TCPListener)}, server)
+	return port
+}
+
+type tcpKeepAliveListener struct {
+	*net.TCPListener
+}
+
+func (ln tcpKeepAliveListener) Accept() (c net.Conn, err error) {
+	tc, err := ln.AcceptTCP()
+	if err != nil {
+		return
+	}
+	tc.SetKeepAlive(true)
+	tc.SetKeepAlivePeriod(3 * time.Minute)
+	return tc, nil
 }
